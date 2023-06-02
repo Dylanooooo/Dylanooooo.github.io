7922979c21f96b3d224f513660bf4d55f00a82ff
Dylanooooo <115074175+Dylanooooo@users.noreply.github.com>
2023-06-02 13:03:58 +0200

    button effect gemaakt

--- main.js
+++ main.js
@@ -1,15 +1,10 @@
 let navbar = document.querySelector(".navbar")
-navbar.innerHTML += `<div class="lightball"></div>`
-let lightball = document.querySelector(".lightball")
 const lb = document.createElement("div");
 
-navbar.addEventListener("mousemove", function(e){
+document.body.addEventListener("mousemove", function(e){
     lb.className = "lightball";
     lb.style.left = e.clientX + "px";
     lb.style.top = e.clientY + "px";
     lb.style.position = "absolute";
-    navbar.appendChild(lb);
-
-    lightball.style.top = e.clientY
-    lightball.style.left = e.clientX
+    document.body.appendChild(lb);
 })
