bc1d9c7551ad12e1295d62b6fd7d938e0e9244ad
Dylanooooo <115074175+Dylanooooo@users.noreply.github.com>
2023-06-05 14:28:22 +0200

    ABOUT ME animated background

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 const lb = document.createElement("div");
 
-document.body.addEventListener("mousemove", function(e){
+document.body.addEventListener("mousemove", function (e) {
     lb.className = "lightball";
     lb.style.left = e.clientX + "px";
     lb.style.top = e.clientY + "px";
@@ -10,33 +10,45 @@ document.body.addEventListener("mousemove", function(e){
     document.body.appendChild(lb);
 })
 
-// --------------------------------------------------------
+// Word scramble effect
 
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
 
 let interval = null;
 
-document.querySelector(".welcome").onmouseover = event => {  
-  let iteration = 0;
-  
-  clearInterval(interval);
-  
-  interval = setInterval(() => {
-    event.target.innerText = event.target.innerText
-      .split("")
-      .map((letter, index) => {
-        if(index < iteration) {
-          return event.target.dataset.value[index];
+document.querySelector(".welcome").onmouseover = event => {
+    let iteration = 0;
+
+    clearInterval(interval);
+
+    interval = setInterval(() => {
+        event.target.innerText = event.target.innerText
+            .split("")
+            .map((letter, index) => {
+                if (index < iteration) {
+                    return event.target.dataset.value[index];
+                }
+
+                return letters[Math.floor(Math.random() * 36)]
+            })
+            .join("");
+
+        if (iteration >= event.target.dataset.value.length) {
+            clearInterval(interval);
         }
-      
-        return letters[Math.floor(Math.random() * 36)]
-      })
-      .join("");
-    
-    if(iteration >= event.target.dataset.value.length){ 
-      clearInterval(interval);
-    }
-    
-    iteration += 1 / 3;
-  }, 30);
+
+        iteration += 1 / 3;
+    }, 30);
 }
+
+// -----------------------------------------------------------
+
+let card = document.querySelector(".card")
+let time = 0
+
+setInterval(() => {
+    let x = Math.floor(Math.random()*100)
+    let y = Math.floor(Math.random()*100)
+    card.style.backgroundPosition = `${x}% ${y}%`
+    time = 5000
+}, time)
