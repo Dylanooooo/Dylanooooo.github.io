c8db6666fa1b979a01ea20e1114539ef91657c7f
Dylanooooo <115074175+Dylanooooo@users.noreply.github.com>
2023-06-05 12:31:54 +0200

    gewerkt aan site

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-let navbar = document.querySelector(".navbar")
+// Lightball effect behind cursor
+
 const lb = document.createElement("div");
 
 document.body.addEventListener("mousemove", function(e){
@@ -8,3 +9,34 @@ document.body.addEventListener("mousemove", function(e){
     lb.style.position = "absolute";
     document.body.appendChild(lb);
 })
+
+// --------------------------------------------------------
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+
+let interval = null;
+
+document.querySelector(".welcome").onmouseover = event => {  
+  let iteration = 0;
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return event.target.dataset.value[index];
+        }
+      
+        return letters[Math.floor(Math.random() * 36)]
+      })
+      .join("");
+    
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    
+    iteration += 1 / 3;
+  }, 30);
+}
