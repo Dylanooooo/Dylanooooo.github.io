32426af36f59e08651b70d62f23b2219157512b8
Dylanooooo <115074175+Dylanooooo@users.noreply.github.com>
2024-07-17 03:23:12 +0200

    Rework

--- Projecten/sd22-p04-project-dashboard-team-enderman/JS/card-3.js
+++
@@ -1,7 +0,0 @@
-fetch(`https://rickandmortyapi.com/api/character`)
-    .then(data => data.json())
-    .then(jsonData => fetchRick(jsonData))
-
-function fetchRick(jsonData){
-    console.log(jsonData)
-}
