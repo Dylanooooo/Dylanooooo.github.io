2dc3d5548d37b728e6e992076218c0bdbaffa254
Wei-Lin Kolff <94454879+WeiLinKolff@users.noreply.github.com>
2025-04-09 14:25:45 +0200

    Merge branch 'Lin' into Developer

---
+++ rooster/config.php
@@ -0,0 +1,16 @@
+<?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+$host = "localhost";
+$dbname = "flitz_events";
+$username = "root";
+$password = "";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Databaseverbinding mislukt: " . $e->getMessage());
+}
